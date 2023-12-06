93555fdc19d712b4acd80bb5568fb336dc954203
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2023-12-06 14:23:20 +0530

    refactor: change color of border and ring styles

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -4,10 +4,10 @@ import type { VariantProps } from 'class-variance-authority';
 import type { ButtonProps } from 'react-aria-components';
 import { cn } from '~/utils/cn';
 
-const buttonVarians = cva(
+const buttonVariants = cva(
   [
     'inline-flex items-center justify-center whitespace-nowrap',
-    'focus:outline-none focus:ring-2',
+    'focus:outline-none focus:ring-2 focus:ring-offset-2',
     'disabled:pointer-events-none disabled:opacity-50',
   ],
   {
@@ -27,12 +27,16 @@ const buttonVarians = cva(
     compoundVariants: [
       // BRAND STYLES ----------------------------------------------------------------------
       {
+        color: 'brand',
+        className: ['focus:ring-brand-400'],
+      },
+      {
         variant: 'fill',
         color: 'brand',
         className: [
           'bg-brand-700 text-white text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-brand-800',
-          'focus:bg-brand-800  focus:ring-brand-300 focus:ring-offset-0',
+          'focus:bg-brand-800 ',
         ],
       },
       {
@@ -41,16 +45,16 @@ const buttonVarians = cva(
         className: [
           'bg-brand-100 text-brand-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-brand-200 hover:text-brand-700',
-          'focus:bg-brand-200 focus:text-brand-700 focus:ring-brand-300 focus:ring-offset-0',
+          'focus:bg-brand-200 focus:text-brand-700',
         ],
       },
       {
         variant: 'outline',
         color: 'brand',
         className: [
-          'bg-transparent text-brand-700 border border-brand-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
+          'bg-transparent text-brand-700 border border-brand-300 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-brand-100',
-          'focus:bg-brand-100 focus:ring-brand-300 focus:ring-offset-0',
+          'focus:bg-brand-100',
         ],
       },
       {
@@ -59,17 +63,21 @@ const buttonVarians = cva(
         className: [
           'bg-transparent text-brand-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-brand-100',
-          'focus:bg-brand-100 focus:ring-brand-300 focus:ring-offset-0',
+          'focus:bg-brand-100',
         ],
       },
       // NEUTRAL STYLES ----------------------------------------------------------------------
+      {
+        color: 'neutral',
+        className: ['focus:ring-neutral-400'],
+      },
       {
         variant: 'fill',
         color: 'neutral',
         className: [
           'bg-neutral-700 text-white text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-neutral-800',
-          'focus:bg-neutral-800  focus:ring-neutral-300 focus:ring-offset-0',
+          'focus:bg-neutral-800 ',
         ],
       },
       {
@@ -78,16 +86,16 @@ const buttonVarians = cva(
         className: [
           'bg-neutral-100 text-neutral-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-neutral-200 hover:text-neutral-700',
-          'focus:bg-neutral-200 focus:text-neutral-700 focus:ring-neutral-300 focus:ring-offset-0',
+          'focus:bg-neutral-200 focus:text-neutral-700',
         ],
       },
       {
         variant: 'outline',
         color: 'neutral',
         className: [
-          'bg-transparent text-neutral-700 border border-neutral-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
+          'bg-transparent text-neutral-700 border border-neutral-300 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-neutral-100',
-          'focus:bg-neutral-100 focus:ring-neutral-300 focus:ring-offset-0',
+          'focus:bg-neutral-100',
         ],
       },
       {
@@ -96,17 +104,21 @@ const buttonVarians = cva(
         className: [
           'bg-transparent text-neutral-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-neutral-100',
-          'focus:bg-neutral-100 focus:ring-neutral-300 focus:ring-offset-0',
+          'focus:bg-neutral-100',
         ],
       },
       // ERROR STYLES ----------------------------------------------------------------------
+      {
+        color: 'error',
+        className: ['focus:ring-error-400'],
+      },
       {
         variant: 'fill',
         color: 'error',
         className: [
           'bg-error-700 text-white text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-error-800',
-          'focus:bg-error-800  focus:ring-error-300 focus:ring-offset-0',
+          'focus:bg-error-800 ',
         ],
       },
       {
@@ -115,16 +127,16 @@ const buttonVarians = cva(
         className: [
           'bg-error-100 text-error-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-error-200 hover:text-error-700',
-          'focus:bg-error-200 focus:text-error-700 focus:ring-error-300 focus:ring-offset-0',
+          'focus:bg-error-200 focus:text-error-700',
         ],
       },
       {
         variant: 'outline',
         color: 'error',
         className: [
-          'bg-transparent text-error-700 border border-error-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
+          'bg-transparent text-error-700 border border-error-300 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-error-100',
-          'focus:bg-error-100 focus:ring-error-300 focus:ring-offset-0',
+          'focus:bg-error-100',
         ],
       },
       {
@@ -133,7 +145,7 @@ const buttonVarians = cva(
         className: [
           'bg-transparent text-error-700 text-sm font-medium h-10 px-4 py-2 rounded-md',
           'hover:bg-error-100',
-          'focus:bg-error-100 focus:ring-error-300 focus:ring-offset-0',
+          'focus:bg-error-100',
         ],
       },
     ],
@@ -144,13 +156,13 @@ const buttonVarians = cva(
   },
 );
 
-type _ButtonProps = ButtonProps & VariantProps<typeof buttonVarians>;
+type _ButtonProps = ButtonProps & VariantProps<typeof buttonVariants>;
 
 const _Button = ({ className, variant, color, ...props }: _ButtonProps) => {
   return (
     <Button
       {...props}
-      className={cn(buttonVarians({ variant, color, className }))}
+      className={cn(buttonVariants({ variant, color, className }))}
     />
   );
 };
