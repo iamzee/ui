6e437a01ab1225e8824dbb220e7b46cbaa5d4bb0
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2023-12-07 23:19:16 +0530

    fear: add support for text tabs

--- app/components/ui/tab.tsx
+++ app/components/ui/tab.tsx
@@ -1,10 +1,11 @@
 /**
- * TODO: add support for text tabs (new variant)
  * TODO: add support for vertical tabs
  * TODO: add support for different sizes
  * TODO: export types
  */
 
+import { cva } from 'class-variance-authority';
+import { createContext, useContext } from 'react';
 import { Tabs, TabList, Tab, TabPanel } from 'react-aria-components';
 import type {
   TabListProps,
@@ -14,11 +15,33 @@ import type {
 } from 'react-aria-components';
 import { cn } from '~/utils/cn';
 
-type _TabsProps = TabsProps;
-
-const _Tabs = ({ className, ...props }: _TabsProps) => {
-  return <Tabs {...props} className={cn('', className)} />;
+type TTabContext = {
+  variant?: 'fill' | 'text';
 };
+
+const TabContext = createContext<TTabContext>({ variant: 'fill' });
+
+type _TabsProps = TabsProps & TTabContext;
+
+const _Tabs = ({ className, variant, ...props }: _TabsProps) => {
+  return (
+    <TabContext.Provider value={{ variant }}>
+      <Tabs {...props} className={cn('', className)} />
+    </TabContext.Provider>
+  );
+};
+
+const tabListVariants = cva('inline-flex h-10 items-center justify-center', {
+  variants: {
+    variant: {
+      fill: 'bg-neutral-100 p-1 rounded',
+      text: 'bg-white',
+    },
+  },
+  defaultVariants: {
+    variant: 'fill',
+  },
+});
 
 type _TabListProps<T> = TabListProps<T>;
 
@@ -26,33 +49,47 @@ const _TabList = <T extends object>({
   className,
   ...props
 }: _TabListProps<T>) => {
+  const { variant } = useContext(TabContext);
   return (
     <TabList
       {...props}
-      className={cn(
-        'inline-flex h-10 items-center justify-center rounded bg-neutral-100 p-1',
-        className,
-      )}
+      className={cn(tabListVariants({ variant, className }))}
     />
   );
 };
 
+const tabVariants = cva(
+  [
+    'inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium text-neutral-400 h-full w-full rounded',
+    'disabled:pointer-events-none disabled:opacity-50',
+    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-500 focus-visible:ring-offset-white',
+    // 'transition-all',
+  ],
+  {
+    variants: {
+      variant: {
+        fill: [
+          'selected:bg-white selected:text-neutral-900 selected:shadow-sm selected:outline-none',
+          'focus-visible:ring-offset-2',
+        ],
+        text: [
+          "relative after:absolute after:content-[''] after:bottom-0 after:h-[2px] after:w-full after:bg-neutral-200",
+          'data-[selected]:after:bg-brand-500 data-[selected]:outline-none data-[selected]:text-neutral-900',
+          'focus-visible:ring-offset-0 data-[focus-visible]:after:h-0',
+        ],
+      },
+    },
+    defaultVariants: {
+      variant: 'fill',
+    },
+  },
+);
+
 type _TabProps = TabProps;
 
 const _Tab = ({ className, ...props }: _TabProps) => {
-  return (
-    <Tab
-      {...props}
-      className={cn(
-        'inline-flex items-center justify-center whitespace-nowrap rounded px-3 py-1.5 text-sm font-medium text-neutral-400',
-        'selected:bg-white selected:text-neutral-900 selected:shadow-sm selected:outline-none',
-        'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-brand-500 focus-visible:ring-offset-white',
-        'disabled:pointer-events-none disabled:opacity-50',
-        'transition-all',
-        className,
-      )}
-    />
-  );
+  const { variant } = useContext(TabContext);
+  return <Tab {...props} className={cn(tabVariants({ variant, className }))} />;
 };
 
 type _TabPanelProps = TabPanelProps;
